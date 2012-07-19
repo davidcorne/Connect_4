5ae6266085648f3fc577539250a82071e27f05a4
David Corne <davidcorne@gmail.com>
2012-07-19 14:32:12 +0100

    Left High Diagonal Win
    Tests pass on diagonal win, left high.

--- Engine/source/src/Engine.cpp
+++ Engine/source/src/Engine.cpp
@@ -139,6 +139,27 @@ int Engine::state()
     }
   }
 
+  
+  // check if there is a diagonal win, left high
+  for (int i = 0; i < m_columns; ++i) {
+    for (int j = 0; j < m_rows; ++j) {
+      // continue if it will go outside of the bounds
+      if (i + m_win_number > m_columns || j + m_win_number > m_rows) {
+        continue;
+      }
+      // if the token at the start of where we are checking is taken is filled
+      game_end = (m_board[i][j] != 0);
+      for (int k = 0; k < m_win_number - 1; ++k) {
+        // check the next tokens are equal
+        game_end =
+          game_end && (m_board[i + k][j + k] == m_board[i + k + 1][j + k + 1]);
+      }
+      if (game_end) {
+        return m_board[i][j];
+      }
+    }
+  }
+
   // return so it compiles
   return 0;
 }
